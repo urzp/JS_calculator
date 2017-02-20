21c151a5735feee5880d38c02c8e73a18e716bc9
urzp <urzp002@mail.ru>
2017-02-20 21:12:00 +0300

    js canculate complite

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,90 @@
 function add(a,b) {
-  return a+b;  
+  return parseInt(a)+parseInt(b);  
 };
 
 function multiply(a,b) {
-  return a*b;  
+  return parseInt(a)*parseInt(b);   
 };
 
 function divide(a,b) {
-  return a/b;  
+  return parseInt(a)/parseInt(b);  
 };
 
 function subtract(a,b) {
-  return a-b;  
+  return parseInt(a)-parseInt(b);   
 };
+
+function calculate(a,b,sign) {
+    if  (sign == "+"){return add(a,b)};
+    if  (sign == "-"){return subtract(a,b)};
+    if  (sign == "/"){return divide(a,b)};
+    if  (sign == "*"){return multiply(a,b)};
+};
+
+function Operation() {  
+    this.str_member="";
+    this.member = 0;
+    this.member_operators = new Array;
+    this.result = null;
+    
+    
+    this.get_member = function(sunmol){
+        this.str_member+=sunmol;
+        this.member = parseInt(this.str_member);
+        //alert(this.member); 
+    };  
+    
+    this.negative_numb = function(){
+       this.member = -1*this.member;
+       alert(this.member); 
+    };
+    
+    
+    this.action = function(sign) {
+        this.fill_member_operators(sign);
+        this.str_member = ""; 
+        this.member = 0;
+        //alert(this.member_operators);
+    };
+    
+     this.count_result = function() {
+         this.member_operators[this.member_operators.length] = this.member;
+         //alert(this.member_operators);
+         this.result = this.member_operators.splice(0,1);
+         
+         while (this.member_operators.length > 1 ){
+             var removed = this.member_operators.splice(0,2);
+             var a = this.result;
+             var b = removed[1];
+             var sign = removed[0];
+             this.result = calculate(a,b,sign);
+         };
+         alert(this.result);
+         this.clear();
+     };
+    
+    this.clear =function() {
+        this.str_member = ""; 
+        this.member = 0;
+        this.result = null;
+        this.member_operators = new Array;
+        //alert(this.member_operators);
+    };
+    
+
+    this.fill_member_operators = function(sign){
+        if (this.str_member != ""){
+            this.member_operators[this.member_operators.length] = this.member;
+            this.member_operators[this.member_operators.length] = sign;
+        } else {
+            this.member_operators[this.member_operators.length -1] = sign;    
+        }; 
+    };
+    
+    
+    
+};
+
+
+var operation = new Operation();
+
